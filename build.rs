5d4c84decc235b1e6f769e81d80a82c96359b2d3
Thomas BESSOU <thomas.bessou@hotmail.fr>
2020-01-11 14:36:15 +0100

    Indent build.rs with spaces

--- build.rs
+++ build.rs
@@ -1,14 +1,14 @@
 fn main() {
-	let btree_multiset_code = std::fs::read_to_string("./src/hash_multiset.rs")
-		.expect("Could not open hash_multiset source file")
-		.replace("Hash + Eq", "Ord")
-		.replace("Eq + Hash", "Ord")
-		.replace("hash_map::", "btree_map::")
-		.replace("HashMap", "BTreeMap")
-		.replace("HashMultiSet", "BTreeMultiSet")
-		.replace("use std::hash::Hash;\n", "")
-		.replace("hash-based multiset", "tree-based multiset");
-	std::fs::write("./src/btree_multiset.rs", btree_multiset_code.as_bytes())
-		.expect("Could not write btree_multiset file");
-	println!("cargo:rerun-if-changed=./src/hash_multiset.rs");
+    let btree_multiset_code = std::fs::read_to_string("./src/hash_multiset.rs")
+        .expect("Could not open hash_multiset source file")
+        .replace("Hash + Eq", "Ord")
+        .replace("Eq + Hash", "Ord")
+        .replace("hash_map::", "btree_map::")
+        .replace("HashMap", "BTreeMap")
+        .replace("HashMultiSet", "BTreeMultiSet")
+        .replace("use std::hash::Hash;\n", "")
+        .replace("hash-based multiset", "tree-based multiset");
+    std::fs::write("./src/btree_multiset.rs", btree_multiset_code.as_bytes())
+        .expect("Could not write btree_multiset file");
+    println!("cargo:rerun-if-changed=./src/hash_multiset.rs");
 }
